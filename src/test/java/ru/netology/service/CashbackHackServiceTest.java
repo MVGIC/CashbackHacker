a31612d146cc66154d6984fa33d856dd43d55789
MVGIC <ex3ct@mail.ru>
2021-03-24 00:00:56 +0300

    small fixes#2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,15 +7,27 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
+    public void shouldRemain100IfAmountIs900(){
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
     public void shouldRemain200IfAmountIs1800() {
         CashbackHackService service = new CashbackHackService();
 
-        int amount = 1000;
+        int amount = 1800;
 
         int actual = service.remain(1800);
-        int exprected = 200;
+        int expected = 200;
 
-        assertEquals(actual,exprected);
+        assertEquals(actual,expected);
     }
 
     @Test
@@ -25,20 +37,8 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = service.remain(1000);
-        int exprected = 0;
+        int expected = 0;
 
-        assertEquals(actual,exprected);
-    }
-
-    @Test
-    public void shouldRemain100IfAmountIs900(){
-        CashbackHackService service = new CashbackHackService();
-
-        int amount = 1000;
-
-        int actual = service.remain(900);
-        int exprected = 100;
-
-        assertEquals(actual,exprected);
+        assertEquals(actual,expected);
     }
 }
