27d8d11b77525f15c2a0d2d1a53dcedd6034eaf7
MVGIC <ex3ct@mail.ru>
2021-03-24 00:09:48 +0300

    small fixes

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,18 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
+    public void shouldRemain300IfAmountIs1700(){
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1700;
+
+        int actual = service.remain(1700);
+        int expected = 300;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
     public void shouldRemain500IfAmountIs500() {
         CashbackHackService service = new CashbackHackService();
 
@@ -26,18 +38,6 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(1000);
         int expected = 0;
-
-        assertEquals(expected,actual);
-    }
-
-    @Test
-    public void shouldRemain300IfAmountIs1700(){
-        CashbackHackService service = new CashbackHackService();
-
-        int amount = 1700;
-
-        int actual = service.remain(1700);
-        int expected = 300;
 
         assertEquals(expected,actual);
     }
